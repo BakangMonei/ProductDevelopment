70f8fbfa0a7b9fc44a3a1ecc72ba0f6df9a34466
BakangMonei <bakangmonei2@gmail.com>
2024-03-17 02:19:49 +0200

    Created new Profile UI

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import DummyImage from "../assets/images/neizatheedev.png";
+import CoverOne from "../assets/images/cover-01.png";
+import userSix from "../assets/images/user/user-06.png";
 import { auth, firestore } from "../firebase"; // Assuming this is the correct path to your firebase.js file
+import SuperAdminNavBar from "./SuperAdminNavBar";
 
 
 const Profile = () => {
   
   const [currentUser, setCurrentUser] = useState(null);
+  const [firstname, setFirstname] = useState("");
   const [userDetails, setUserDetails] = useState(null);
 
   useEffect(() => {
@@ -14,12 +19,14 @@ const Profile = () => {
       if (user) {
         // User is signed in.
         setCurrentUser(user);
+        setFirstname(user);
         
         // Fetch user details from each collection
         fetchUserDetails(user.uid);
       } else {
         // No user is signed in.
         setCurrentUser(null);
+        setFirstname(null);
         setUserDetails(null);
       }
     });
@@ -41,8 +48,7 @@ const Profile = () => {
         ...userDetailsA,
         ...userDetailsB,
         ...userDetailsC,
-        // ...userDetailsD
-        // Add more fields as needed
+
       };
 
       // Set user details state with the combined data
@@ -76,149 +82,101 @@ const Profile = () => {
   }
 
   return (
-    <div className=" justify-center items-center"> 
-      <section class="  h-screen bg-gray-100/50 justify-center items-center">
-        <form class="container max-w-2xl mx-auto shadow-md md:w-3/4">
-          <div class="p-4 border-t-2 border-indigo-400 rounded-lg bg-gray-100/5 ">
-            <div class="max-w-sm mx-auto md:w-full md:mx-0">
-              <div class="inline-flex items-center space-x-4">
-                <a href="#" class="relative block">
-                  <img
-                    alt="profil"
-                    src={DummyImage}
-                    class="mx-auto object-cover rounded-full h-16 w-16 "
-                  />
-                  
-                </a>
-                <h1 className="text-gray-600">{currentUser.displayName}</h1>
-              </div>
-            </div>
-          </div>
-          <div class="space-y-6 bg-white">
-            <div class="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-              <h2 class="max-w-sm mx-auto md:w-1/3">Account</h2>
-              <div class="max-w-sm mx-auto md:w-2/3">
-                <div class=" relative ">
-                  <input
-                    type="text"
-                    id="user-info-email"
-                    class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                    placeholder="Email"
-                    value={currentUser.email}
-                  />
-                </div>
-              </div>
-            </div>
-            <hr />
-            <div class="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-              <h2 class="max-w-sm mx-auto md:w-1/3">Personal info</h2>
-              <div class="max-w-sm mx-auto space-y-5 md:w-2/3">
-                <div>
-                  <div class=" relative ">
-                    <input
-                      type="text"
-                      id="user-info-name"
-                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                      placeholder="FirstName"
-                      value={currentUser.firstname}
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div class=" relative ">
-                    <input
-                      type="text"
-                      id="user-info-name"
-                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                      placeholder="LastName"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div class=" relative ">
-                    <input
-                      type="text"
-                      id="user-info-phone"
-                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                      placeholder="Phone number"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-            
-            <div class="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-              <h2 class="max-w-sm mx-auto md:w-1/3">Others</h2>
-              <div class="max-w-sm mx-auto space-y-5 md:w-2/3">
-                <div>
-                  <div class=" relative ">
-                    <input
-                      type="text"
-                      id="user-info-name"
-                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                      placeholder="Country"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div class=" relative ">
-                    <input
-                      type="text"
-                      id="user-info-name"
-                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                      placeholder="Sport"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div class=" relative ">
-                    <input
-                      type="text"
-                      id="user-info-phone"
-                      class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                      placeholder="Gender"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
+    <div className="flex">
+      <div>
+        <SuperAdminNavBar/>
+      </div>
 
-            <hr />
-            <div class="items-center w-full p-8 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-              <h2 class="max-w-sm mx-auto md:w-4/12">Change password</h2>
-              <div class="w-full max-w-sm pl-2 mx-auto space-y-5 md:w-5/12 md:pl-9 md:inline-flex">
-                <div class=" relative ">
-                  <input
-                    type="text"
-                    id="user-info-password"
-                    class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                    placeholder="Password"
-                  />
-                </div>
-                
-              </div>
-              <div class="text-center md:w-3/12 md:pl-6">
-                <button
-                  type="button"
-                  class="py-2 px-4  bg-pink-600 hover:bg-pink-700 focus:ring-pink-500 focus:ring-offset-pink-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+      <div>
+      <div className=" overflow-hidden rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+       <div className="relative z-20 h-35 md:h-65">
+          <img
+            src={CoverOne}
+            alt="profile cover"
+            className="h-full w-full rounded-tl-sm rounded-tr-sm object-cover object-center"
+          />
+          <div className="absolute bottom-1 right-1 z-10 xsm:bottom-4 xsm:right-4">
+            <label
+              htmlFor="cover"
+              className="flex cursor-pointer items-center justify-center gap-2 rounded bg-primary py-1 px-2 text-sm font-medium text-white hover:bg-opacity-90 xsm:px-4"
+            >
+              <input type="file" name="cover" id="cover" className="sr-only" />
+              <span>
+                <svg
+                  className="fill-current"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  Change
-                </button>
-              </div>
-            </div>
-            
-            <hr />
-            <div class="w-full px-4 pb-4 ml-auto text-gray-500 md:w-1/3">
-              <button
-                type="submit"
-                class="py-2 px-4  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M4.76464 1.42638C4.87283 1.2641 5.05496 1.16663 5.25 1.16663H8.75C8.94504 1.16663 9.12717 1.2641 9.23536 1.42638L10.2289 2.91663H12.25C12.7141 2.91663 13.1592 3.101 13.4874 3.42919C13.8156 3.75738 14 4.2025 14 4.66663V11.0833C14 11.5474 13.8156 11.9925 13.4874 12.3207C13.1592 12.6489 12.7141 12.8333 12.25 12.8333H1.75C1.28587 12.8333 0.840752 12.6489 0.512563 12.3207C0.184375 11.9925 0 11.5474 0 11.0833V4.66663C0 4.2025 0.184374 3.75738 0.512563 3.42919C0.840752 3.101 1.28587 2.91663 1.75 2.91663H3.77114L4.76464 1.42638ZM5.56219 2.33329L4.5687 3.82353C4.46051 3.98582 4.27837 4.08329 4.08333 4.08329H1.75C1.59529 4.08329 1.44692 4.14475 1.33752 4.25415C1.22812 4.36354 1.16667 4.51192 1.16667 4.66663V11.0833C1.16667 11.238 1.22812 11.3864 1.33752 11.4958C1.44692 11.6052 1.59529 11.6666 1.75 11.6666H12.25C12.4047 11.6666 12.5531 11.6052 12.6625 11.4958C12.7719 11.3864 12.8333 11.238 12.8333 11.0833V4.66663C12.8333 4.51192 12.7719 4.36354 12.6625 4.25415C12.5531 4.14475 12.4047 4.08329 12.25 4.08329H9.91667C9.72163 4.08329 9.53949 3.98582 9.4313 3.82353L8.43781 2.33329H5.56219Z"
+                    fill="white"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M6.99992 5.83329C6.03342 5.83329 5.24992 6.61679 5.24992 7.58329C5.24992 8.54979 6.03342 9.33329 6.99992 9.33329C7.96642 9.33329 8.74992 8.54979 8.74992 7.58329C8.74992 6.61679 7.96642 5.83329 6.99992 5.83329ZM4.08325 7.58329C4.08325 5.97246 5.38909 4.66663 6.99992 4.66663C8.61075 4.66663 9.91659 5.97246 9.91659 7.58329C9.91659 9.19412 8.61075 10.5 6.99992 10.5C5.38909 10.5 4.08325 9.19412 4.08325 7.58329Z"
+                    fill="white"
+                  />
+                </svg>
+              </span>
+              <span>Edit</span>
+            </label>
+          </div>
+        </div>
+       </div>
+       <div className="px-4 pb-6 text-center lg:pb-8 xl:pb-11.5">
+          <div className="relative z-30 mx-auto -mt-22 h-30 w-full max-w-30 rounded-full bg-white/20 p-1 backdrop-blur sm:h-44 sm:max-w-44 sm:p-3">
+            <div className="relative drop-shadow-2 border">
+              <img src={DummyImage} alt="profile" />
+              <label
+                htmlFor="profile"
+                className="absolute bottom-0 right-0 flex h-8.5 w-8.5 cursor-pointer items-center justify-center rounded-full bg-primary text-white hover:bg-opacity-90 sm:bottom-2 sm:right-2"
               >
-                Save
-              </button>
+                <svg
+                  className="fill-current"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M4.76464 1.42638C4.87283 1.2641 5.05496 1.16663 5.25 1.16663H8.75C8.94504 1.16663 9.12717 1.2641 9.23536 1.42638L10.2289 2.91663H12.25C12.7141 2.91663 13.1592 3.101 13.4874 3.42919C13.8156 3.75738 14 4.2025 14 4.66663V11.0833C14 11.5474 13.8156 11.9925 13.4874 12.3207C13.1592 12.6489 12.7141 12.8333 12.25 12.8333H1.75C1.28587 12.8333 0.840752 12.6489 0.512563 12.3207C0.184375 11.9925 0 11.5474 0 11.0833V4.66663C0 4.2025 0.184374 3.75738 0.512563 3.42919C0.840752 3.101 1.28587 2.91663 1.75 2.91663H3.77114L4.76464 1.42638ZM5.56219 2.33329L4.5687 3.82353C4.46051 3.98582 4.27837 4.08329 4.08333 4.08329H1.75C1.59529 4.08329 1.44692 4.14475 1.33752 4.25415C1.22812 4.36354 1.16667 4.51192 1.16667 4.66663V11.0833C1.16667 11.238 1.22812 11.3864 1.33752 11.4958C1.44692 11.6052 1.59529 11.6666 1.75 11.6666H12.25C12.4047 11.6666 12.5531 11.6052 12.6625 11.4958C12.7719 11.3864 12.8333 11.238 12.8333 11.0833V4.66663C12.8333 4.51192 12.7719 4.36354 12.6625 4.25415C12.5531 4.14475 12.4047 4.08329 12.25 4.08329H9.91667C9.72163 4.08329 9.53949 3.98582 9.4313 3.82353L8.43781 2.33329H5.56219Z"
+                    fill=""
+                  />
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M7.00004 5.83329C6.03354 5.83329 5.25004 6.61679 5.25004 7.58329C5.25004 8.54979 6.03354 9.33329 7.00004 9.33329C7.96654 9.33329 8.75004 8.54979 8.75004 7.58329C8.75004 6.61679 7.96654 5.83329 7.00004 5.83329ZM4.08337 7.58329C4.08337 5.97246 5.38921 4.66663 7.00004 4.66663C8.61087 4.66663 9.91671 5.97246 9.91671 7.58329C9.91671 9.19412 8.61087 10.5 7.00004 10.5C5.38921 10.5 4.08337 9.19412 4.08337 7.58329Z"
+                    fill=""
+                  />
+                </svg>
+                <input
+                  type="file"
+                  name="profile"
+                  id="profile"
+                  className="sr-only"
+                />
+              </label>
             </div>
           </div>
-        </form>
-      </section>
+          <div className="mt-4">
+            <h3 className="mb-1.5 text-2xl font-semibold text-black dark:text-white">
+            {currentUser.email}
+            </h3>
+            <p className="font-medium">Super Administrator</p>
+
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
