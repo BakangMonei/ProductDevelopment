e5e574e0c24ab7155dbc352e3ef52e77d34243a0
BakangMonei <bakangmonei2@gmail.com>
2024-04-04 05:31:45 +0200

    fix vid

--- src/components/BroadCastorComponents/ViewUploadedVideos.js
+++ src/components/BroadCastorComponents/ViewUploadedVideos.js
@@ -9,11 +9,16 @@ import {
 import { firestore } from "../../firebase"; // Import your firebase configuration
 
 const ViewUploadedVideos = () => {
-    return(
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div>
+      <Thumbnail video={video} />
+      <a href={video.url}>
+        <h3>{video.title}</h3>
+        <p>{video.description}</p>
+      </a>
+      <LikeButton video={video} />
+    </div>
+  );
+};
 
 export default ViewUploadedVideos;
