bc66052bfbed1827c19c9a6eb58dc40ed144a551
BakangMonei <bakangmonei2@gmail.com>
2024-05-15 16:16:23 +0200

    Fixed the profile

--- src/components/Forms/UserProfile.js
+++ src/components/Forms/UserProfile.js
@@ -317,45 +317,6 @@ const UserProfile = () => {
                         />
                       </div>
                     </div>
-                    <div className="mb-5.5">
-                      <label
-                        className="mb-3 block text-sm font-medium text-black dark:text-white"
-                        htmlFor="sports"
-                      >
-                        Sports
-                      </label>
-                      <div className="relative flex items-center">
-                        <span className="mr-3">
-                          <svg
-                            className="fill-current"
-                            width="20"
-                            height="20"
-                            viewBox="0 0 20 20"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              clipRule="evenodd"
-                              d="M4.1 38.2C1.4 34.2 0 29.4 0 24.6C0 11 11 0 24.6 0H133.9c11.2 0 21.7 5.9 27.4 15.5l68.5 114.1c-48.2 6.1-91.3 28.6-123.4 61.9L4.1 38.2zm503.7 0L405.6 191.5c-32.1-33.3-75.2-55.8-123.4-61.9L350.7 15.5C356.5 5.9 366.9 0 378.1 0H487.4C501 0 512 11 512 24.6c0 4.8-1.4 9.6-4.1 13.6zM80 336a176 176 0 1 1 352 0A176 176 0 1 1 80 336zm184.4-94.9c-3.4-7-13.3-7-16.8 0l-22.4 45.4c-1.4 2.8-4 4.7-7 5.1L168 298.9c-7.7 1.1-10.7 10.5-5.2 16l36.3 35.4c2.2 2.2 3.2 5.2 2.7 8.3l-8.6 49.9c-1.3 7.6 6.7 13.5 13.6 9.9l44.8-23.6c2.7-1.4 6-1.4 8.7 0l44.8 23.6c6.9 3.6 14.9-2.2 13.6-9.9l-8.6-49.9c-.5-3 .5-6.1 2.7-8.3l36.3-35.4c5.6-5.4 2.5-14.8-5.2-16l-50.1-7.3c-3-.4-5.7-2.4-7-5.1l-22.4-45.4z"
-                              fill=""
-                            />
-                          </svg>
-                        </span>
-                        <select
-                          className="w-full rounded border border-stroke bg-gray py-3 pl-4 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
-                          name="sports"
-                          id="sports"
-                          disabled
-                          value={user.sport}
-                        >
-                          <option value="Football">Football</option>
-                          <option value="Basketball">Basketball</option>
-                          <option value="Tennis">Tennis</option>
-                          {/* Add more options as needed */}
-                        </select>
-                      </div>
-                    </div>
 
                     <div className="mb-5.5">
                       <label
@@ -382,20 +343,55 @@ const UserProfile = () => {
                             />
                           </svg>
                         </span>
-                        <select
+                        <input
                           className="w-full rounded border border-stroke bg-gray py-3 pl-4 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                          type="text"
                           name="gender"
                           id="gender"
+                          placeholder="gender"
                           value={user.gender}
                           disabled
-                        >
-                          <option value="Male">Male</option>
-                          <option value="Female">Female</option>
-                          <option value="Other">Other</option>
-                          {/* Add more options as needed */}
-                        </select>
+                        />
                       </div>
                     </div>
+
+                    <div className="mb-5.5">
+                      <label
+                        className="mb-3 block text-sm font-medium text-black dark:text-white"
+                        htmlFor="sport"
+                      >
+                        Sports
+                      </label>
+                      <div className="relative flex items-center">
+                        <span className="mr-3">
+                          <svg
+                            className="fill-current"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 20 20"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M10 17.5C13.6467 17.5 16.875 14.3482 16.875 10.5C16.875 6.65176 13.6467 3.5 10 3.5C6.35327 3.5 3.125 6.65176 3.125 10.5C3.125 14.3482 6.35327 17.5 10 17.5ZM10 14.5C7.23858 14.5 5 12.2614 5 10C5 7.73858 7.23858 5.5 10 5.5C12.7614 5.5 15 7.73858 15 10C15 12.2614 12.7614 14.5 10 14.5Z"
+                              fill=""
+                            />
+                          </svg>
+                        </span>
+                        <input
+                          className="w-full rounded border border-stroke bg-gray py-3 pl-4 pr-4.5 text-black focus:border-primary focus-visible:outline-none dark:border-strokedark dark:bg-meta-4 dark:text-white dark:focus:border-primary"
+                          type="text"
+                          name="sport"
+                          id="sport"
+                          placeholder="sport"
+                          value={user.sport}
+                          disabled
+                        />
+                      </div>
+                    </div>
+                    
 
                     <div className="mb-5.5">
                       <label
