aa9ece45ef358786462e4b6d41ac22331e94c599
BakangMonei <bakangmonei2@gmail.com>
2024-05-20 15:56:57 +0200

    gfyfjjgfhg

---
+++ src/components/Charts/StatisticsChart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const StatisticsChart = () => {
+    return (
+        <div>
+        <h1>Statistics Chart</h1>
+        </div>
+    );
+}
+
+
+export default StatisticsChart;
--- src/pages/superadmin/SuperAdminDashboard.js
+++ src/pages/superadmin/SuperAdminDashboard.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
 import SuperAdminNavBar from "../../components/NavBars/SuperAdminNavBar";
-import TableUsers from "../../components/data/TableUsers";
-import TableBroadcasters from "../../components/data/TableBroadcasters";
-
+// import { TableUsers } from "../../components/data"; // import TableUsers
+// import { TableBroadcasters } from "../../components/data"; // import TableBroadcasters
+import { LineChart, CartesianGrid, XAxis, YAxis } from "recharts"; // import from recharts
+import LoginChart from "../../components/Charts/LoginChart";
+import SportsWatchedChart from "../../components/Charts/SportsWatchedChart";
+import StatisticsChart from "../../components/Charts/StatisticsChart";
+import AdminTable from "../../components/tables/AdminTable";
 
 export const SuperAdminDashboard = () => {
+  const [admins, setAdmins] = useState([]);
+
+  // useEffect(() => {
+  //   // fetch data from Firebase
+  // }, []);
+
   return (
     <div className="flex flex-auto p-3">
       <div>
@@ -12,10 +22,13 @@ export const SuperAdminDashboard = () => {
       </div>
 
       <div className="p-3">
-        <TableUsers/>
-        <TableBroadcasters/>
-      </div>
+        <div className="flex flex-wrap">
+          <div className="w-full md:w-1/3 xl:w-1/4 p-3">
+            <LoginChart />
+          </div>
+        </div>
 
+      </div>
     </div>
   );
 };
