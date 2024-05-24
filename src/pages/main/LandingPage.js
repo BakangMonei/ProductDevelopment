bb0da673e1ee183129961b6a946c13c0df845358
BakangMonei <bakangmonei2@gmail.com>
2024-05-24 02:24:08 +0200

    Doneeeeeeeee

--- src/pages/main/LandingPage.js
+++ src/pages/main/LandingPage.js
@@ -1,18 +1,26 @@
-import React from "react";
+import Navigation from "../../components/NavBars/Navigation";
+import Hero from "../../components/NavBars/hero";
+import mobilehero from "../../assets/images/image-hero-mobile.png";
+import desktophero from "../../assets/images/image-hero-desktop.png";
+import baaagggggg from "../../assets/images/bg2.jpg";
 
-
-const LandingPage = () => {
-
-
-    return (
-        <div>
-        <h1>Landing Page</h1>
+function LandingPage() {
+  return (
+    <div className=" font-epilogue bg-[hsl(0,0%,98%)]">
+      <div className=" flex flex-col">
+        <Navigation />
+        <img src={baaagggggg} alt="image-hero-mobile" className="lg:hidden" />
+        <div className=" lg:flex lg:mx-44 lg:gap-4 lg:mt-10">
+          <img
+            src={baaagggggg}
+            alt="image-hero-desktop"
+            className="hidden lg:flex lg:order-2 max-w-full max-h-screen overflow-auto"
+          />
+          <Hero />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
-
-
-
 
 export default LandingPage;
